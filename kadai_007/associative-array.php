390b527aa0d78b011eff158b21625802b7a96ea8
cTomi0630 <154723423+cTomi0630@users.noreply.github.com>
2024-01-15 20:16:43 +0900

    第7章課題
    第7章課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>第7章課題</title>
+</head>
+<body>
+  <p>
+    <?php
+      $array_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($array_data);
+    ?>
+  </p>
+</body>
+</html>
